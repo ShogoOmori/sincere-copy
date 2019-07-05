995fab226ea47b7e57f75eb6c13e95044e9b9834
ShogoOmori <omori.s@xscore.co.jp>
2019-07-05 16:36:50 +0900

    created header

--- components/Header.vue
+++ components/Header.vue
@@ -1,135 +1,78 @@
 <template>
-  <header>
-    <div>
-      <h1><i><img src="~static/images/ic_book.png" alt="ブック"></i>ETC</h1>
-      <address>
-        <i><img src="~static/images/ic_tel.png" alt="電話"></i>
-        <div>
-          <a href="#">0120-123-123</a>
-          <dl>
-            <dt>受付時間｜</dt>
-            <dd>
-              <time>9:30</time>~<time>20:00</time>
-            </dd>
-            <dd>土日祝日を除く</dd>
-          </dl>
-        </div>
-      </address>
-      <a class="st-button" href="#sc-form">
-        お問い合わせはこちら
-      </a>
+  <header class="light">
+    <div class="header-left">
+      <i>
+        <img src="~static/images/ic_book.png" alt="株式会社ETC">
+      </i>
+      <a href="#">ETC</a>
+    </div>
+    <div class="header-right">
+      <i>
+        <img src="~static/images/ic_tel.png" alt="電話番号はこちら">
+      </i>
+      <div class="st-contact">
+        <address>
+          <a href="tel:0120-123-123">0123-123-123</a>
+          <p>受付時間 | 9:30~20:00 土日祝日を除く</p>
+        </address>
+      </div>
+      <a href="#" class="st-button">お問い合わせはこちら</a>
     </div>
   </header>
 </template>
 <style lang="scss" scoped>
 header {
-  background-color: #00d0d4;
-  min-width: 1000px;
-  > div {
-    width: 965px;
-    height: 73px;
-    margin: 0 auto;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  height: 80px;
+}
+.header-left {
+  margin-right: 350px;
     display: flex;
     align-items: center;
-    > a {
-      width: 210px;
-      height: 45px;
-    }
-  }
-}
-i {
-  display: grid;
-}
-h1 {
-  display: inline-flex;
-  align-items: center;
-  font-size: 32px;
-  margin-right: auto;
-  color: #fff;
+    justify-content: center;
   i {
-    margin-right: 14px;
-  }
-}
-address {
-  display: flex;
-  align-items: center;
-  margin-right: 20px;
-  i {
-    margin-right: 5px;
-  }
-  > div {
-    display: flex;
-    flex-direction: column;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
   }
   a {
-    font-size: 35px;
+    line-height: 40px;
     color: #fff;
-    line-height: 1;
+    font-size: 40px;
     font-weight: bold;
   }
-  dl {
-    display: flex;
+}
+.header-right {
+  display: flex;
+  align-items:center;
+  i {
+    display: inline-flex;
     align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    img {
+      margin-right: 10px;
+    }
   }
-  dt,
-  dd,
-  time {
-    font-size: 12px;
+}
+.st-contact {
+  a {
+    font-size: 35px;
+    font-weight: bold;
     color: #fff;
-    line-height: 1.5;
   }
-  dd:last-child {
-    margin-left: 10px;
-  }
-}
-@media screen and (max-width: 780px) {
-  header {
-    width: 100%;
-    min-width: auto;
-    > div {
-      width: 90%;
-      margin: 0 auto;
-      display: flex;
-      align-items: center;
-    }
+  p {
+    font-size: 12px;
   }
 }
-@media screen and (max-width: 680px) {
-  header {
-    > div {
-      height: 16vw;
-      > a {
-        width: 40vw;
-        height: 45px;
-        box-sizing: border-box;
-      }
-    }
-  }
-  h1 {
-    font-size: 5.3333vw;
-    i {
-      width: 5.5vw;
-      margin-right: 1.66vw;
-      img {
-        width: 100%;
-      }
-    }
-  }
-  address {
-    margin-right: 2.6666vw;
-    a {
-      font-size: 4.26666vw;
-    }
-    i {
-      width: 12vw;
-      margin-right: 0;
-      img {
-        width: 100%;
-      }
-    }
-    > div {
-      display: none;
-    }
-  }
+.st-button {
+  height: 50px;
+  padding: 0 30px;
+  margin-left: 10px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <my-header />
-    <main>
+    <!-- <main>
       <my-main />
       <my-inform />
       <my-button />
@@ -15,39 +15,39 @@
       <my-example />
     </main>
     <my-form />
-    <my-footer />
+    <my-footer /> -->
   </div>
 </template>
 <script>
 import MyHeader from '~/components/Header.vue'
-import MyMain from '~/components/Main.vue'
-import MyInform from '~/components/Information.vue'
-import MyMerit from '~/components/Merit.vue'
-import MyAccept from '~/components/Accept.vue'
-import MyPerform from '~/components/Performance.vue'
-import MyService from '~/components/Service.vue'
-import MyPrice from '~/components/Price.vue'
-import MyFlow from '~/components/Flow.vue'
-import MyExample from '~/components/Example.vue'
-import MyForm from '~/components/Form.vue'
-import MyFooter from '~/components/Footer.vue'
-import MyButton from '~/components/Button.vue'
+// import MyMain from '~/components/Main.vue'
+// import MyInform from '~/components/Information.vue'
+// import MyMerit from '~/components/Merit.vue'
+// import MyAccept from '~/components/Accept.vue'
+// import MyPerform from '~/components/Performance.vue'
+// import MyService from '~/components/Service.vue'
+// import MyPrice from '~/components/Price.vue'
+// import MyFlow from '~/components/Flow.vue'
+// import MyExample from '~/components/Example.vue'
+// import MyForm from '~/components/Form.vue'
+// import MyFooter from '~/components/Footer.vue'
+// import MyButton from '~/components/Button.vue'
 
 export default {
   components: {
-    MyHeader,
-    MyMain,
-    MyInform,
-    MyMerit,
-    MyAccept,
-    MyPerform,
-    MyService,
-    MyPrice,
-    MyFlow,
-    MyExample,
-    MyForm,
-    MyFooter,
-    MyButton
+    MyHeader
+    // MyMain,
+    // MyInform,
+    // MyMerit,
+    // MyAccept,
+    // MyPerform,
+    // MyService,
+    // MyPrice,
+    // MyFlow,
+    // MyExample,
+    // MyForm,
+    // MyFooter,
+    // MyButton
   }
 }
 </script>
