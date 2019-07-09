0543dcd5e21cd06abaf21b684c8fd3ede2006808
ShogoOmori <omori.s@xscore.co.jp>
2019-07-09 15:49:11 +0900

    created performance

---
+++ components/Performance.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h1 class="title light">厚いご信頼を支える膨大な実績</h1>
+    <h2 class="st-apeal text-blue"><strong>弁護士法人ETCは解決実績8000件以上!!</strong></h2>
+    <div>
+      <img src="~static/images/ref_perform01.jpg" alt="笑顔の人">
+      <img src="~static/images/ref_perform02.jpg" alt="真顔の人">
+      <img src="~static/images/ref_perform03.jpg" alt="ボール持っている人">
+      <img src="~static/images/ref_perform04.jpg" alt="笑顔の女性">
+    </div>
+    <p class="st-text">
+      <em class="text-light">むちうちの人も治療中の人</em>も、<em class="text-light">後遺障害認定が難しいと言われた人</em>も、<br>
+      どんなことでもご相談ください。<br>
+      <em class="text-light">ETC独自のリーガルスタッフシステム</em>で、弁護士とスタッフがタッグを組んで、<br>
+      事故状況を徹底的に聞き取り、洗い直して調査することで、有利な証拠を発見します。
+    </p>
+  </section>
+</template>
+<style lang="scss" scoped>
+section {
+  padding-bottom: 70px;
+}
+.st-apeal {
+  text-align: center;
+  font-weight: bold;
+  font-size: 30px;
+}
+div {
+  margin-top: 30px;
+  display: flex;
+}
+.st-text {
+  text-align: center;
+  margin-top: 30px;
+}
+</style>
