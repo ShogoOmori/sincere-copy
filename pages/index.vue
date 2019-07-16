24d035b6ab5924c89833989db0604ddf44be17fd
ShogoOmori <omori.s@xscore.co.jp>
2019-07-16 11:10:59 +0900

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <my-header />
     <main>
       <my-main />
-      <!-- <my-inform />
+      <my-inform />
       <my-button />
       <my-merit />
       <my-button />
@@ -12,7 +12,7 @@
       <my-service />
       <my-price />
       <my-flow />
-      <my-example /> -->
+      <my-example />
     </main>
     <my-form />
     <my-footer />
@@ -21,33 +21,33 @@
 <script>
 import MyHeader from '~/components/Header.vue'
 import MyMain from '~/components/Main.vue'
-// import MyInform from '~/components/Information.vue'
-// import MyMerit from '~/components/Merit.vue'
-// import MyAccept from '~/components/Accept.vue'
-// import MyPerform from '~/components/Performance.vue'
-// import MyService from '~/components/Service.vue'
-// import MyPrice from '~/components/Price.vue'
-// import MyFlow from '~/components/Flow.vue'
-// import MyExample from '~/components/Example.vue'
-// import MyForm from '~/components/Form.vue'
-// import MyFooter from '~/components/Footer.vue'
-// import MyButton from '~/components/Button.vue'
+import MyInform from '~/components/Information.vue'
+import MyMerit from '~/components/Merit.vue'
+import MyAccept from '~/components/Accept.vue'
+import MyPerform from '~/components/Performance.vue'
+import MyService from '~/components/Service.vue'
+import MyPrice from '~/components/Price.vue'
+import MyFlow from '~/components/Flow.vue'
+import MyExample from '~/components/Example.vue'
+import MyForm from '~/components/Form.vue'
+import MyFooter from '~/components/Footer.vue'
+import MyButton from '~/components/Button.vue'
 
 export default {
   components: {
     MyHeader,
-    MyMain
-    // MyInform,
-    // MyMerit,
-    // MyAccept,
-    // MyPerform,
-    // MyService,
-    // MyPrice,
-    // MyFlow,
-    // MyExample,
-    // MyForm,
-    // MyFooter,
-    // MyButton
+    MyMain,
+    MyInform,
+    MyMerit,
+    MyAccept,
+    MyPerform,
+    MyService,
+    MyPrice,
+    MyFlow,
+    MyExample,
+    MyForm,
+    MyFooter,
+    MyButton
   }
 }
 </script>
