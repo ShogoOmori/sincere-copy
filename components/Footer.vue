4a53532fb916cbc815bc8467fbdd2248d11cbaac
ShogoOmori <omori.s@xscore.co.jp>
2019-07-16 11:09:45 +0900

    created footer

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="light">
+    <i>Copyright (c) 2018 ETC,Inc All Rights Reserved.</i>
+  </footer>
+</template>
+<style lang="scss" scoped>
+footer {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+}
+</style>
